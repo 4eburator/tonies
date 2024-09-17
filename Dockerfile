cbfd1dfb436ac456ca82f350311e18a0d7dc3bf7
Vitalii Sigov <vsigov@yahoo.com>
2024-09-17 11:40:41 +0200

    work in progress

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-# Dockerfile
 FROM openjdk:8-jdk-slim
 
 # Set environment variables for Spark
@@ -13,17 +12,16 @@ RUN apt-get update && apt-get install -y curl python3 python3-pip && \
     rm spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz
 
 # Download Open Search connector for Spark:
-RUN curl -O https://repo1.maven.org/maven2/org/elasticsearch/elasticsearch-spark-30_2.12/8.15.1/elasticsearch-spark-30_2.12-8.15.1.jar
+ENV ELASTIC_SEARCH_SPARK_VERSION=30_2.12
+ENV ELASTIC_SEARCH_CONNECTOR_VERSION=8.15.1
+RUN curl -O https://repo1.maven.org/maven2/org/elasticsearch/elasticsearch-spark-${ELASTIC_SEARCH_SPARK_VERSION}/8.15.1/elasticsearch-spark-${ELASTIC_SEARCH_SPARK_VERSION}-${ELASTIC_SEARCH_CONNECTOR_VERSION}.jar
 RUN mkdir /opt/spark-app
-RUN mv ./elasticsearch-spark-30_2.12-8.15.1.jar /opt/spark-app/elasticsearch-hadoop.jar
+RUN mv ./elasticsearch-spark-${ELASTIC_SEARCH_SPARK_VERSION}-${ELASTIC_SEARCH_CONNECTOR_VERSION}.jar /opt/spark-app/elasticsearch-hadoop.jar
 
 # Copy the Spark application to the container
-# COPY ./etl/spark_app.py /opt/spark-app/
 COPY ./etl/tonies_etl.py /opt/spark-app/
 
 WORKDIR /opt/spark
 
 # Set the entrypoint to run Spark
-# ENTRYPOINT ["./bin/spark-submit", "--jars", "/opt/spark-app/elasticsearch-hadoop.jar", "/opt/spark-app/spark_app.py"]
 ENTRYPOINT ["./bin/spark-submit", "--jars", "/opt/spark-app/elasticsearch-hadoop.jar", "/opt/spark-app/tonies_etl.py"]
-
--- etl/tonies_etl.py
+++ etl/tonies_etl.py
@@ -16,81 +16,69 @@ spark.sparkContext.setLogLevel("DEBUG")
 
 # Read events from OpenSearch
 df_events = spark.read \
-    .format("org.elasticsearch.spark.sql") \
-    .option("es.resource", "toniebox-events") \
+    .format('org.elasticsearch.spark.sql') \
+    .option('es.resource', 'toniebox-events') \
     .load()
 
 # Ensure the timestamp field is in the correct format
-df_events = df_events.withColumn(
-    "timestamp",
-    to_timestamp(col("timestamp"))
-)
+df_events = df_events.withColumn('timestamp', to_timestamp(col('timestamp')))
 
 # Define start and stop event IDs
 start_event_ids = [12, 100, 131, 534]
 stop_event_ids = [33, 887]
 
 # Filter start and stop events
-df_start_events = df_events.filter(col("event_id").isin(start_event_ids))
-df_stop_events = df_events.filter(col("event_id").isin(stop_event_ids))
+df_start_events = df_events.filter(col('event_id').isin(start_event_ids))
+df_stop_events = df_events.filter(col('event_id').isin(stop_event_ids))
 
 # Assign row numbers to start and stop events for pairing
-window_spec = Window.partitionBy("mac", "tonie_id").orderBy("timestamp")
+window_spec = Window.partitionBy('mac', 'tonie_id').orderBy('timestamp')
 
-df_start_events = df_start_events.withColumn(
-    "event_rank",
-    row_number().over(window_spec)
-)
-df_stop_events = df_stop_events.withColumn(
-    "event_rank",
-    row_number().over(window_spec)
+df_start_events = df_start_events.withColumn('event_rank',row_number().over(window_spec))
+df_stop_events = df_stop_events.withColumn('event_rank', row_number().over(window_spec)
 )
 
 # Alias dataframes for clarity in join
-df_start_events = df_start_events.alias("start")
-df_stop_events = df_stop_events.alias("stop")
+df_start_events = df_start_events.alias('start')
+df_stop_events = df_stop_events.alias('stop')
 
 # Join start and stop events
 df_playbacks = df_start_events.join(
     df_stop_events,
     on=[
-        col("start.mac") == col("stop.mac"),
-        col("start.tonie_id") == col("stop.tonie_id"),
-        col("start.event_rank") == col("stop.event_rank")
+        col('start.mac') == col('stop.mac'),
+        col('start.tonie_id') == col('stop.tonie_id'),
+        col('start.event_rank') == col('stop.event_rank')
     ],
     how='inner'
 )
 
 # Calculate playback duration in seconds
 df_playbacks = df_playbacks.withColumn(
-    "playback_duration",
-    (col("stop.timestamp").cast("long") - col("start.timestamp").cast("long"))
+    'playback_duration',
+    (col('stop.timestamp').cast('long') - col('start.timestamp').cast('long'))
 )
 
 # Filter out negative or zero durations
-df_playbacks = df_playbacks.filter(col("playback_duration") > 0)
+df_playbacks = df_playbacks.filter(col('playback_duration') > 0)
 
 # Select and rename columns for the final dataset
 df_playbacks = df_playbacks.select(
-    col("start.mac").alias("mac"),
-    col("start.tonie_id").alias("tonie_id"),
-    col("start.timestamp").alias("playback_start"),
-    col("stop.timestamp").alias("playback_end"),
-    col("playback_duration")
+    col('start.mac').alias('mac'),
+    col('start.tonie_id').alias('tonie_id'),
+    col('start.timestamp').alias('playback_start'),
+    col('stop.timestamp').alias('playback_end'),
+    col('playback_duration')
 )
 
 # Write the output to OpenSearch
 df_playbacks.write \
-    .format("org.elasticsearch.spark.sql") \
-    .option("es.resource", "toniebox-playbacks") \
-    .mode("overwrite") \
+    .format('org.elasticsearch.spark.sql') \
+    .option('es.resource', 'toniebox-playbacks') \
+    .mode('overwrite') \
     .save()
 
 
+df_playbacks.write.csv('/opt/spark-apps/playbacks_output.csv', header=True)
 
-df_playbacks.write.csv("/opt/spark-apps/playbacks_output.csv", header=True)
-
-
-# Stop the Spark session
 spark.stop()
-
